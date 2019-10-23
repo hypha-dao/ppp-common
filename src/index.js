920cd3a40ebd82d81e6662f306da9216f812399c
Sebastian <smontero@mightywizards.com>
2019-10-23 12:49:33 -0500

    Added CommMethods to the main index file to be exported

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import {
     ProfileFetchTypes,
     PublicFields,
     RootFields,
+    CommMethods,
 } from './const';
 
 export {
     PublicFields,
     ProfileFetchTypes,
     RootFields,
+    CommMethods,
 };
