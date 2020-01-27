df9b14334e13f1920302a50a84f642a5cbe745ab
Sebastian <smontero@mightywizards.com>
2020-01-27 12:30:19 -0600

    Added isValid method to the OauthTokenStatus const class. Added isEnabled method to the OauthAppStatus class

--- src/const/OauthAppStatus.js
+++ src/const/OauthAppStatus.js
@@ -2,4 +2,7 @@ export default {
     ENABLED: 'Enabled',
     DISABLED_BY_APP: 'Disabled by app',
     DISABLED_BY_SERVER: 'Disabled by server',
+    isEnabled(status) {
+        return status === this.ENABLED;
+    }
 };
--- src/const/OauthTokenStatus.js
+++ src/const/OauthTokenStatus.js
@@ -2,4 +2,7 @@ export default {
     VALID: 'Valid',
     INVALIDATED_BY_USER: 'Invalidated by user',
     INVALIDATED_BY_SERVER: 'Invalidated by server',
+    isValid(status) {
+        return status === this.VALID;
+    }
 };
