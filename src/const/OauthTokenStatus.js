7b56f18796bb04cc71a425fb94589e39d47ae128
Sebastian <smontero@mightywizards.com>
2020-01-29 11:59:39 -0600

    Added Revoked by app token status

--- src/const/OauthTokenStatus.js
+++ src/const/OauthTokenStatus.js
@@ -1,8 +1,9 @@
 export default {
     VALID: 'Valid',
     REVOKED_BY_USER: 'Revoked by user',
+    REVOKED_BY_APP: 'Revoked by app',
     REVOKED_BY_SERVER: 'Revoked by server',
     isValid(status) {
         return status === this.VALID;
-    }
+    },
 };
