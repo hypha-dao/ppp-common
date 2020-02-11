ba2c9ac420b8ffe682284a59cc973be4d6d9de30
Sebastian <smontero@mightywizards.com>
2020-02-11 14:28:08 -0600

    Added APP_DELETED OauthTokenStatus const

--- src/const/OauthTokenStatus.js
+++ src/const/OauthTokenStatus.js
@@ -3,6 +3,7 @@ export default {
     REVOKED_BY_USER: 'Revoked by user',
     REVOKED_BY_APP: 'Revoked by app',
     REVOKED_BY_SERVER: 'Revoked by server',
+    APP_DELETED: 'App was deleted',
     isValid(status) {
         return status === this.VALID;
     },
